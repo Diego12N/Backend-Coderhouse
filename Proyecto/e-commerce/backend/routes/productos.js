cff720afdc94c4e01820ec1427fd01871957efa7
Diego Emmanuel Naranjo <diego.emma.@outlook.com>
2022-07-03 19:56:56 -0300

    Clase 14 mas Entrega Finalizada

--- Proyecto/e-commerce/backend/routes/productos.js
+++ Proyecto/e-commerce/backend/routes/productos.js
@@ -7,24 +7,38 @@ const {
 	modifyProduct,
 } = require("../helpers/routesHelpers");
 
+const userAdmin = false;
+
+function authorizedAdmin(req, res, next) {
+	if (!userAdmin) {
+		return res.json({
+			error: -1,
+			description: `route ${req.originalUrl} metodo ${req.method} no autorizado`,
+		});
+	}
+
+	next();
+}
+
 routes.get("/:id?", async (req, res) => {
+	// ? indica que el parametro es condicional, por lo que puede venir o no.
 	const id = parseInt(req.params.id);
 	const products = await getProducts(id);
 	res.send(products);
 });
 
-routes.post("/", (req, res) => {
+routes.post("/", authorizedAdmin, (req, res) => {
 	let newProduct = saveProducts(req);
 
 	res.send(newProduct);
 });
 
-routes.put("/:id", async (req, res) => {
+routes.put("/:id", authorizedAdmin, async (req, res) => {
 	const id = parseInt(req.params.id);
 	modifyProduct(req.body, res, id);
 });
 
-routes.delete("/:id", async (req, res) => {
+routes.delete("/:id", authorizedAdmin, async (req, res) => {
 	const id = parseInt(req.params.id);
 
 	const data = await contenedor.deleteById(id);
